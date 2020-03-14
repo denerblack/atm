500eadf453c97c68515d3b5c30538cdc9af4534c
Dener Wilian Pereira do Carmo <dener@deners-air.home>
2020-03-14 17:25:17 -0300

    Adjusting assets static

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ATM
     config.load_defaults 6.0
     config.i18n.available_locales = [:en, :"pt-BR"]
     config.i18n.default_locale = :'pt-BR'
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
