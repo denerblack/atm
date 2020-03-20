acc93c0c295dcca28f5f11da38761fea9f54841f
Dener Wilian Pereira do Carmo <dener@deners-air.home>
2020-03-20 00:05:43 -0300

    Entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 
 cd /app
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
-bundle exec rake db:migrate
-bundle exec rake assets:precompile
+#bundle exec rake db:migrate
+#bundle exec rake assets:precompile
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
 
 exec "$@"
